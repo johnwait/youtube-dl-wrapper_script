2f378209b7445a1962f7b90b99956491e3c113d0
johnwait <jonathan.brochu@gmail.com>
2019-08-21 00:04:18 -0400

    Historical commit: v1_r3

--- youtube-dl_run.cmd
+++ youtube-dl_run.cmd
@@ -5,36 +5,36 @@ set __self=%0
 if (%1)==(:) if not (%2)==() shift & shift & goto %2
 
 :: Config
-:: - Default values, peths, etc.
-set l_default_vidh=
-set l_ytd_path=.
 set l_ytd_exe=youtube-dl.exe
+:: - Default paths & other values
 set l_ffmpeg_path=C:\Program Files\ffmpeg-4.0.2-win64-static\bin
+set l_default_vidh=
 :: - Query phrases
 set "l_query_url=Enter or paste the URL here: "
 set "l_query_filebase=Enter the filename (without extension): "
-set "l_query_vidh=Preferred height to download (default: %l_vidh%): "
+set "l_query_vidh=Preferred height to download (default: %l_default_vidh%): "
 
-:: Set up workspace; we want to be working within the temporary folder in case
-:: we finish with left-overs (undeleted part files, after cancelations, etc.)
-if "%l_ytd_path%"=="" set "l_ytd_path=%cd%"
-set path=%path%;%l_ytd_path%\%l_ytd_exe%
-pushd "%tmp%"
+:: Add parent folder to system paths
+set path=%path%;%cd%
+:: Navigate to output path
+pushd "%l_output_path%"
 
-:: Payload
+:: Start download process
 set /p "l_url=%l_query_url%"
 echo:
 set /p "l_filebase=%l_query_filebase%"
 echo:
+if "%l_filebase%"=="" %l_ytd_exe% -F "%l_url%"&pause&goto cleanup
 set l_vidh=%l_default_vidh%
 set /p "l_vidh=%l_query_vidh%"
 echo:
-if "%l_vidh%"=="" %l_ytd_exe% -o "%l_filebase%.%%(ext)s" --ffmpeg-location "%l_ffmpeg_path%" %l_url%
-if not "%l_vidh%"=="" %l_ytd_exe% -f "best[height=%l_vidh%]" -o "%l_filebase%.%%(ext)s" --ffmpeg-location "%l_ffmpeg_path%" %l_url%
+if "%l_vidh%"=="" %l_ytd_exe% -o "%l_filebase%.%%(ext)s" --ffmpeg-location "%l_ffmpeg_path%" "%l_url%"
+if not "%l_vidh%"=="" %l_ytd_exe% -f "best[height=%l_vidh%][ext=mp4]+bestaudio" -o "%l_filebase%.%%(ext)s" --ffmpeg-location "%l_ffmpeg_path%" "%l_url%"
 echo:
 pause
 
 :cleanup
+popd
 set l_url=
 set l_filebase=
 set l_vidh=
